08dbc42a29f76af19054f27c53b671679bd7686a
nahid <hasannahid371@gmail.com>
2017-11-29 00:53:42 +0600

    generic class included

---
+++ HRM/HRM.Data/HRMDbContext.cs
@@ -0,0 +1,53 @@
+﻿using HRM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Data
+{
+    public class HRMDbContext : DbContext
+    {
+        public DbSet<AdvertiserList> AdvertiserLists { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Bonus> Bonuses { get; set; }
+        public DbSet<Bonuses> LBonuses { get; set; }
+        public DbSet<CircularTemplate> CircularTemplates { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<ConfirmationStatus> ConfirmationStatuses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<DepartmentHistory> DepartmentHistories { get; set; }
+        public DbSet<Designation> Designations { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeBio> EmployeeBios { get; set; }
+        public DbSet<EmployeeHistory> EmployeeHistories { get; set; }
+        public DbSet<EmployeePerformenceMetric> EmployeePerformenceMetrics { get;set;}
+        public DbSet<EmployeeSalary> EmployeeSalaries { get; set; }
+        public DbSet<HireRequest> HireRequests { get; set; }
+        public DbSet<HireThread> HireThreads { get; set; }
+        public DbSet<Interview> Interviews { get; set; }
+        public DbSet<Interviewee> Interviewees{ get; set;}
+        public DbSet<LeaveApplication> LeaveApplications { get;set;}
+        public DbSet<LeaveApplicationCategory> LeaveApplicationCatagories { get; set; }
+        public DbSet<Notice> Notices { get; set; }
+        public DbSet<Privilege> Privileges { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<SalaryComponents> LsalaryComponents { get; set; }
+        public DbSet<Salgrade> Salgrades { get; set; }
+        public DbSet<SalgradeHistory> SalgradeHistories{ get; set; }
+        public DbSet<SupportingDocument> SupportingDocuments { get; set; }
+        public DbSet<TemporaryCV> TemporaryCVs { get; set; }
+        public DbSet<Training> Trainings { get; set; }
+
+        //bhuji nai eta niye ghata lagbe
+        /*public HRMDbContext()
+        {
+            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }*/
+
+
+
+    }
+}
---
+++ HRM/HRM.Data/Interfaces/IEmployeeReposatory.cs
@@ -0,0 +1,14 @@
+﻿using HRM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Data.Interfaces
+{
+    public interface IEmployeeReposatory : IReposatory<Employee>
+    {
+        //extra method for employee class
+    }
+}
