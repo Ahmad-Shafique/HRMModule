8830a1e72a827bb0af76ff902561e455d236d1fe
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2017-12-12 01:34:42 +0600

    added test layer

---
+++ HRM/HRM.Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using HRM.Entity;
+using HRM.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRM.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdvertiserList adl = new AdvertiserList();
+            adl.AdvertiserName = "New Test Advertiser List name";
+            adl.ContactInfo = "???@advertiser.com";
+            adl.Description = "weird guy with proper reach";
+
+            AdvertiserListService service = new AdvertiserListService();
+
+            service.Insert(adl);
+
+            Console.WriteLine("Commands have been executed. Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
