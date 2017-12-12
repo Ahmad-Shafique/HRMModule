d7ed4331fa8a1bd8a50683021e7cf23c51cc7f65
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2017-12-12 23:58:41 +0600

    Designation Controller and CRUD views added

---
+++ HRM/HRM/Controllers/DesignationsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HRM.Data;
+using HRM.Entity;
+using HRM.Service.Interfaces;
+using HRM.Service;
+
+namespace HRM.Controllers
+{
+    public class DesignationsController : Controller
+    {
+        private IDomainService<Designation> service = new ServiceFactory().Create<Designation>();
+
+        // GET: Designations
+        public async Task<ActionResult> Index()
+        {
+            return View(await service.GetAll());
+        }
+
+        // GET: Designations/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Designation designation = await service.Get(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // GET: Designations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Designations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "DesignationId,DesignationName")] Designation designation)
+        {
+            if (ModelState.IsValid)
+            {
+                await service.Insert(designation);
+                return RedirectToAction("Index");
+            }
+
+            return View(designation);
+        }
+
+        // GET: Designations/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Designation designation = await service.Get(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // POST: Designations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "DesignationId,DesignationName")] Designation designation)
+        {
+            if (ModelState.IsValid)
+            {
+                Designation temp = await service.Get(designation.DesignationId);
+                await service.RemoveByEntity(temp);
+                await service.Insert(designation);
+            }
+            return View(designation);
+        }
+
+        // GET: Designations/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Designation designation = await service.Get(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // POST: Designations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Designation item = await service.Get(id);
+            await service.RemoveByEntity(item);
+            return RedirectToAction("Index");
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
