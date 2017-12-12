df1428dbe1f39e64963b2f7f09bee7b99f0b2974
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2017-12-12 22:03:18 +0600

    Added CRUD views for notice comments

---
+++ HRM/HRM/Controllers/CommentsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HRM.Data;
+using HRM.Entity;
+using HRM.Service;
+using HRM.Service.Interfaces;
+
+namespace HRM.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private IDomainService<Comment> service = new ServiceFactory().Create<Comment>();
+
+        // GET: Comments
+        public async Task<ActionResult> Index()
+        {
+            return View(await service.GetAll());
+        }
+
+        // GET: Comments/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = await service.Get(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // GET: Comments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "CommentId,CommentText,EmployeeId,Noticeid")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                await service.Insert(comment);
+                return RedirectToAction("Index");
+            }
+
+            return View(comment);
+        }
+
+        // GET: Comments/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = await service.Get(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "CommentId,CommentText,EmployeeId,Noticeid")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                Comment temp = await service.Get(comment.CommentId);
+                await service.RemoveByEntity(temp);
+                await service.Insert(comment);
+            }
+            return View(comment);
+        }
+
+        // GET: Comments/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = await service.Get(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Comment comment = await service.Get(id);
+            await service.RemoveByEntity(comment);
+            return RedirectToAction("Index");
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
