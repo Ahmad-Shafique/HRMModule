45839345c9fea579aa7cdc838be8046c258720ce
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2017-12-12 22:23:09 +0600

    Department controller and CRUD views added

---
+++ HRM/HRM/Controllers/DepartmentsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HRM.Data;
+using HRM.Entity;
+using HRM.Service.Interfaces;
+using HRM.Service;
+
+namespace HRM.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private IDomainService<Department> service = new ServiceFactory().Create<Department>();
+
+        // GET: Departments
+        public async Task<ActionResult> Index()
+        {
+            return View(await service.GetAll());
+        }
+
+        // GET: Departments/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = await service.Get(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // GET: Departments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "DepartmentId,DepartmentName,DepartmentLocation,DepartmentDescription,DepartmentHeadId")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                await service.Insert(department);
+                return RedirectToAction("Index");
+            }
+
+            return View(department);
+        }
+
+        // GET: Departments/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = await service.Get(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Departments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "DepartmentId,DepartmentName,DepartmentLocation,DepartmentDescription,DepartmentHeadId")] Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                Department temp = await service.Get(department.DepartmentId);
+                await service.RemoveByEntity(temp);
+                await service.Insert(department);
+            }
+            return View(department);
+        }
+
+        // GET: Departments/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = await service.Get(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Departments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Department department = await service.Get(id);
+            await service.RemoveByEntity(department);
+            return RedirectToAction("Index");
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
