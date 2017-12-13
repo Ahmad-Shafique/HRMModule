0552ecdd94f9c2615ca9a04c7f566a1fe6e712b9
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2017-12-13 14:34:22 +0600

    Added Controllers and CRUD views for SupportingDocument, TemporaryCV, Training, TrainingEmployee

---
+++ HRM/HRM/Controllers/TrainingsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HRM.Data;
+using HRM.Entity;
+using HRM.Service;
+using HRM.Service.Interfaces;
+
+namespace HRM.Controllers
+{
+    public class TrainingsController : Controller
+    {
+        private IDomainService<Training> service = new ServiceFactory().Create<Training>();
+
+
+        public async Task<ActionResult> Index()
+        {
+            return View(await service.GetAll());
+        }
+
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Training entity = await service.Get(id);
+
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // ****************************************************************************************************************************************************************
+        public async Task<ActionResult> Create([Bind(Include = "TrainingId,TrainingName,TrainingDescription,StartDate,EndDate,SuccessRate")] Training entity)
+        {
+            if (ModelState.IsValid)
+            {
+                await service.Insert(entity);
+                return RedirectToAction("Index");
+            }
+
+            return View(entity);
+        }
+
+
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Training entity = await service.Get(id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // ******************************************************************************************************************************************************************
+        public async Task<ActionResult> Edit([Bind(Include = "TrainingId,TrainingName,TrainingDescription,StartDate,EndDate,SuccessRate")] Training entity)
+        {
+            if (ModelState.IsValid)
+            {
+                // **********************************************************************************************************************************************************
+                Training temp = await service.Get(entity.TrainingId);
+                await service.RemoveByEntity(temp);
+                await service.Insert(entity);
+            }
+            /////// return View(Training);
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Training entity = await service.Get(id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Training entity = await service.Get(id);
+            await service.RemoveByEntity(entity);
+            return RedirectToAction("Index");
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
