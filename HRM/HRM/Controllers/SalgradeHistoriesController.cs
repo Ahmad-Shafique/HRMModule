a743c7e87f38c2def99c18f55fb0136ed8a00651
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2017-12-13 14:21:29 +0600

    Added controllers and CRUD views for

---
+++ HRM/HRM/Controllers/SalgradeHistoriesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HRM.Data;
+using HRM.Entity;
+using HRM.Service;
+using HRM.Service.Interfaces;
+
+namespace HRM.Controllers
+{
+    public class SalgradeHistoriesController : Controller
+    {
+        private IDomainService<SalgradeHistory> service = new ServiceFactory().Create<SalgradeHistory>();
+
+
+        public async Task<ActionResult> Index()
+        {
+            return View(await service.GetAll());
+        }
+
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SalgradeHistory entity = await service.Get(id);
+
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // ****************************************************************************************************************************************************************
+        public async Task<ActionResult> Create([Bind(Include = "SalgradeHistoryId,ColumnName,PreviousValue,NewValue,Date,SalgradetId")] SalgradeHistory entity)
+        {
+            if (ModelState.IsValid)
+            {
+                await service.Insert(entity);
+                return RedirectToAction("Index");
+            }
+
+            return View(entity);
+        }
+
+
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SalgradeHistory entity = await service.Get(id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // ******************************************************************************************************************************************************************
+        public async Task<ActionResult> Edit([Bind(Include = "SalgradeHistoryId,ColumnName,PreviousValue,NewValue,Date,SalgradetId")] SalgradeHistory entity)
+        {
+            if (ModelState.IsValid)
+            {
+                // **********************************************************************************************************************************************************
+                SalgradeHistory temp = await service.Get(entity.SalgradeHistoryId);
+                await service.RemoveByEntity(temp);
+                await service.Insert(entity);
+            }
+            /////// return View(SalgradeHistory);
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SalgradeHistory entity = await service.Get(id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            SalgradeHistory entity = await service.Get(id);
+            await service.RemoveByEntity(entity);
+            return RedirectToAction("Index");
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
---
+++ HRM/HRM/Controllers/SalgradesController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HRM.Data;
+using HRM.Entity;
+using HRM.Service;
+using HRM.Service.Interfaces;
+
+namespace HRM.Controllers
+{
+    public class SalgradesController : Controller
+    {
+        private IDomainService<Salgrade> service = new ServiceFactory().Create<Salgrade>();
+
+
+        public async Task<ActionResult> Index()
+        {
+            return View(await service.GetAll());
+        }
+
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salgrade entity = await service.Get(id);
+
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // ****************************************************************************************************************************************************************
+        public async Task<ActionResult> Create([Bind(Include = "SalgradeId,GradeName,LowSalary,HighSalary")] Salgrade entity)
+        {
+            if (ModelState.IsValid)
+            {
+                await service.Insert(entity);
+                return RedirectToAction("Index");
+            }
+
+            return View(entity);
+        }
+
+
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salgrade entity = await service.Get(id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // ******************************************************************************************************************************************************************
+        public async Task<ActionResult> Edit([Bind(Include = "SalgradeId,GradeName,LowSalary,HighSalary")] Salgrade entity)
+        {
+            if (ModelState.IsValid)
+            {
+                // **********************************************************************************************************************************************************
+                Salgrade temp = await service.Get(entity.SalgradeId);
+                await service.RemoveByEntity(temp);
+                await service.Insert(entity);
+            }
+            /////// return View(Salgrade);
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salgrade entity = await service.Get(id);
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(entity);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Salgrade entity = await service.Get(id);
+            await service.RemoveByEntity(entity);
+            return RedirectToAction("Index");
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
