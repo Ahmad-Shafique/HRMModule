c7dc3c075c1be81522b8c773746b25d4bffc8ae6
Rafat <rafatx003@gmail.com>
2017-12-16 00:03:59 +0600

    created employee display in employee controller. Also an employee.js script for ajax handling which is yet to be done.

---
+++ Static Views/Static views/Scripts/transportationScript.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    //on click of the modal button
+    $('#buttonConfirmTransport').click(function () {
+        //if you just need the ids as CSV in any action jst set the action name in the form in Area.cshtml
+        $('#vehicleForm').submit();
+        //$('#employeeModalDiv').toggle();
+        //$("#bonusSelectedEmployeeDiv").toggle();
+        
+    });
+
+    $('#buttonConfirmEmployee').click(function () {
+        //if you just need the ids as CSV in any action jst set the action name in the form in Area.cshtml
+        $('#employeeForm').submit();
+        //$('#employeeModalDiv').toggle();
+        //$("#bonusSelectedEmployeeDiv").toggle();
+
+    });
+});
