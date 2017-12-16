bef798bc226ac0ea4af6b7f7000dfa6cbda7ac70
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2017-12-17 05:12:48 +0600

    salary paymnet controller and views added

---
+++ HRM/HRM.View/Controllers/SalaryPaymentController.cs
@@ -0,0 +1,21 @@
+﻿using HRM.Service;
+using HRM.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRM.View.Controllers
+{
+    public class SalaryPaymentController : Controller
+    {
+        private ICommonViewServiceInterface Service = ServiceFactory.GetCommonViewService();
+        // GET: SalaryPayment
+        public ActionResult Index()
+        {
+            ViewBag.Salaries = Service.CalculateAllEmployeeTotalSalary();
+            return View();
+        }
+    }
+}
