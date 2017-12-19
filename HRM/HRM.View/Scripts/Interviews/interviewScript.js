32d16754d7ae99a2b26acf72e6d5d53546123f9e
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2017-12-20 05:13:49 +0600

    New view layout

---
+++ HRM/HRM.View/Scripts/Interviews/interviewScript.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+	console.log('Document ready !');
+
+    //on click of the modal button
+    $('#buttonCandidateAddition').click(function () {
+
+	 console.log('Entered button candidate addition');
+
+        //an arraay for containing checkbox values
+	    var resultString = '';
+        var checkBox = $('input[name = "EnabledCandidates"]');
+        $.each(checkBox, function () {
+            if ($(this).is(":checked")) {
+               	resultString.append($(this).val() + ',');
+		console.log('appended !');
+            }
+        });
+
+
+	console.log(resultString);
+
+    });
+
+
+    $('#CreateInterview').click(function () {
+	
+	console.log('Entered button confim transport');
+
+    //var resultString = '';
+
+
+    var schedule = $('#scheduleText').val();
+    console.log('Schedule is: ' + schedule); 
+
+
+	//console.log(resultString);
+
+    });
+
+
+});
