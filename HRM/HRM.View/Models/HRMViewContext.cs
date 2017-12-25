ed867fc5722e25be19cba60ad83338ccfbeccc3f
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2017-12-25 20:55:27 +0600

    Leave section added

--- HRM/HRM.View/Models/HRMViewContext.cs
+++ HRM/HRM.View/Models/HRMViewContext.cs
@@ -20,5 +20,7 @@ namespace HRM.View.Models
         }
 
         public System.Data.Entity.DbSet<HRM.Entity.EmployeeBio> EmployeeBios { get; set; }
+
+        public System.Data.Entity.DbSet<HRM.Entity.LeaveApplication> LeaveApplications { get; set; }
     }
 }
